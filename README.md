0f1e09835d905c8b60263fd1f82369aa86c464c7
Julian Pollinger <julian@pollinger.dev>
2025-04-10 10:06:39 +0200

    init vue3

--- README.md
+++ README.md
@@ -1,41 +1,33 @@
-# Ella Frontend
-[![GitHub release (latest by date)](https://img.shields.io/github/v/release/educorvi/ella_frontend)](https://github.com/educorvi/ella_frontend/releases/latest)
-[![Build](https://github.com/educorvi/ella_frontend/actions/workflows/node.js.yml/badge.svg)](https://github.com/educorvi/ella_frontend/actions/workflows/node.js.yml)
-[![GitHub issues](https://img.shields.io/github/issues/educorvi/ella_frontend)](https://github.com/educorvi/ella_frontend/issues)
-## Project setup
-```
+# ella_frontend
+
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+
+## Type Support for `.vue` Imports in TS
+
+TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) to make the TypeScript language service aware of `.vue` types.
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vite.dev/config/).
+
+## Project Setup
+
+```sh
 npm install
 ```
 
-### Configuration
-#### Grundeinstellungen
-Die Grundeinstellungen können auf zwei Weisen festgelegt werden. Entweder durch den Konfigurator, der automatisch während des Installationsprozesses startet, oder durch Kopieren der Datei `config/ella.config.example.js` nach `config/ella.config.js` und anschließendes manuelles Bearbeiten der letzteren Datei.
-[Hier mehr Details zu den Einstellungen](https://educorvi.github.io/ella_frontend/module-config.html).  
-Der Konfigurator kann durch Löschen von `config/ella.config.js` und anschließendes Ausführen von `npm run createSettings` neu gestartet werden.  
-Ein Beispiel befindet sich in `config/ella.config.example.js`.
-  
-Änderung des Logos
-- Das Logo kann ersetzt werden, indem man die entsprechenden Dateien überschrieben werden. Die Bild sollte mindestens 512x512 Pixel groß sein.
-  - Generelles Logo der App: ``src/assets/AppLogo.png``
-  - Logo für die Navbar: ``src/assets/HeadLogo.png``
-- Sollte das AppLogo ersetzt worden sein, muss anschließend der Befehl ``npm run generateIcons`` ausgeführt werden, um die benötigten Icons aus dem Logo zu generieren.
+### Compile and Hot-Reload for Development
 
-#### Styling
-Im Ordner ``src/theme`` befinden sich nach der Installation die Dateien ``variables.scss`` und ``theme.scss`` in der entsprechend Variablen hinzugefügt und überschrieben werden können, sowie ein Änderungen am Theme gemacht werden können.
-
-Sollte der Ordner ``src/theme`` gelöscht worden sein, muss er mit ``npm run theme:init`` wieder initialisiert werden. Weiterhin gibt es auch einige weitere Befehle zur Theme-Verwaltung, hier eine Übersicht:
-``` bash
-npm run theme:init     #Initialisiert den Theme Ordner
-npm run theme:select   #Stellt alle bereitgestellten Themes zur Auswahl und installiert das gewählte
-npm run theme:update   #Updated ein mittels 'npm run theme:select' installiertes Theme
-npm run theme:install  #Installiert ein Theme von einer URL, die auf eine zip Datei zeigt
+```sh
+npm run dev
 ```
 
-Neue Themes können zur Auswahl bereitgestellt werden, indem sie als Repository in der Educorvi Organization auf Github veröffentlicht werden. Hierbei muss der Name des Repositorys dem Schema ``ef_theme-<name>`` entsprechen. Es wird immer der neueste Release heruntergeladen.
-### Production Build
-```
+### Type-Check, Compile and Minify for Production
+
+```sh
 npm run build
 ```
-
-### Komponentendokumentation
-[https://educorvi.github.io/ella_frontend](https://educorvi.github.io/ella_frontend/)
--- src/App.vue
+++ src/App.vue
@@ -1,121 +1,85 @@
-<template>
-  <div id="root" class="box" v-touch:swipe="swipeHandler">
-    <nav-bar-comp id="top" class="topbar"/>
-    <div id="app" class="main" v-if="mainData && !mainData.error">
-      <div>
-        <router-view/>
-      </div>
-    </div>
-    <div v-else-if="!(mainData && mainData.error)" class="hider">
-      <custom-spinner class="mt-2"/>
-    </div>
-    <div class="mt-5 w-75" v-else>
-      <b-jumbotron bg-variant="danger" :text-variant="textVariant">
-        <template #header>Fehler!</template>
-        <template #lead>Bitte versuchen Sie es später erneut!</template>
-        ({{ mainData.error }})
-      </b-jumbotron>
-    </div>
-
-    <sidebar/>
-  </div>
-</template>
-
-<script>
-import NavBarComp from "/src/components/Navigation/NavBarComp.vue";
-import {mapGetters} from "vuex";
-import CustomSpinner from "/src/components/CustomSpinner.vue";
-import Sidebar from "/src/components/Navigation/Sidebar.vue";
-import {TITLE, THEME_COLOR} from "../config"
-import {isLightColor, getCSSVariable} from "/src/utilities/globals.mjs";
-
-
-/**
- * @module App
- * @description The App's entry point
- * @vue-computed {String} textVariant Determines, if text should be light or dark based on the 'danger' color
- */
-export default {
-  name: 'App',
-  components: {
-    Sidebar,
-    CustomSpinner,
-    NavBarComp
-  },
-  mounted() {
-    /**
-     * This is used to enable File handling of .efa Files
-     * If the Browser supports file handling API, then, if a User opens the file, the PWA will open and the file(s) it was opened with will be queued into the launchqueue.
-     * The following reads the first file, and loads it into the referred service.
-     */
-    if ('launchQueue' in window) {
-      window.launchQueue.setConsumer((launchParams) => {
-        // Nothing to do when the queue is empty.
-        if (!launchParams.files.length) {
-          return;
-        }
-        const fileHandle = launchParams.files[0];
-        fileHandle.getFile().then(res => {
-          res.text().then(text => {
-            this.$store.commit("load", JSON.parse(text));
-            this.$router.push("/services/" + JSON.parse(text).name);
-          })
-        });
-      });
-    }
-  },
-  created() {
-    //Loads the data from the ella-backend and sets title + theme color
-    this.$store.commit("getMainData");
-    document.title = TITLE;
-    const metaThemeColor = document.querySelector("meta[name=theme-color]");
-    metaThemeColor.setAttribute("content", THEME_COLOR);
-  },
-  computed: {
-    textVariant() {
-      return isLightColor(getCSSVariable("danger")) ? 'dark' : 'light';
-    },
-    ...mapGetters(["mainData"])
-  },
-  methods: {
-    /**
-     * Handles swipe events to open and close the sidebar
-     * @param {String} evt The Swipe-Event
-     */
-    swipeHandler(evt) {
-      // Opens sidebar while using slider for audio, so commented out for now
-      // if (evt === "left") {
-      //   this.$store.commit('sidebar', true);
-      // }
-      if (evt === "right") {
-        this.$store.commit('sidebar', false);
-      }
-
-    }
-  }
-}
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style lang="scss">
-@import "styles";
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-.main, .topbar {
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
   width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
 }
 
-#top {
-  position: sticky;
-  left: 0;
-  top: 0;
+nav a.router-link-exact-active {
+  color: var(--color-text);
 }
 
-.box {
-  display: flex;
-  flex-flow: column;
-  align-items: center;
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
 }
 
-.hider {
-  width: 100vw;
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
 }
 </style>
